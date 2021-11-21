507e4760a35e45aab75fcbc888f0acb1fade6a5a
Brian Garland <briangarlandk@gmail.com>
2021-11-20 20:40:32 -0600

    Added two questions to array thus generating  new readme file w/ title & license

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const generateMarkdown = require('./utils/generateMarkdown.js')
+const fs = require('fs')
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'project',
+        message: 'What is the name of your project?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license would you like for your README file?',
+        choices: [
+            'MIT', 'Apache'
+        ]
+    }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('Successfully created README.md')
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    
+    .then((answers)=>{
+        const readmeAnswers = generateMarkdown(answers);
+        console.log(readmeAnswers)
+        writeToFile('./output/README.md', readmeAnswers)
+
+    })
+}
 
 // Function call to initialize app
 init();
---
+++ output/README.md
@@ -0,0 +1,4 @@
+# readme-generator
+  [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+  
